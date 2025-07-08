352094b90a1246df1e8f03a8efc372242316f93c
Richard Essuman <ressuman001@gmail.com>
2025-07-08 09:38:42 +0000

    Adds authentication and onboarding layouts
    Implements new layouts for user authentication and onboarding processes.
    
    Introduces dedicated components for authentication screens including 'welcome', 'sign-up', and 'sign-in'.
    
    Enhances the root layout with navigation for onboarding screens such as 'find-ride', 'confirm-ride', and 'book-ride'.
    
    Updates font usage to include a new font family for improved UI consistency.

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,3 @@
+export default function TabsLayout() {
+  return <div></div>;
+}
---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,22 @@
+import { Stack } from "expo-router";
+
+export default function OnboardingLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="find-ride" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="confirm-ride"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="book-ride"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+}
